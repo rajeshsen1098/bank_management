798164f94f15f2170d917c7fadb2c9aaea3225dc
Rajesh Sen <124055427+rajeshsen1098@users.noreply.github.com>
2024-04-07 01:30:58 +0530

    Update conn.java

--- src/bank_management/conn.java
+++ src/bank_management/conn.java
@@ -1,20 +1,15 @@
 package bank_management;
-import  java.sql.*;
-//import  com.mysql.*;
-
+import  java.sql.*; 
 
 public class Conn {
     Connection c;
     Statement s;
     public Conn(){
         try{
-//            Class.forName(com.mysql.cj.jdbc.Driver);
             c= DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","Rajesh@1098");
             s=c.createStatement();
         } catch (Exception e){
             System.out.println(e);
-
-
         }
     }
 }
